33ff96653a044e345227ea7977c5bb3bdc8901f6
Darrell Tunnell <darrell.tunnell@covarius.com>
2019-06-08 23:29:06 +0100

    more tidyup

--- Sample.Authentication/Program.cs
+++ Sample.Authentication/Program.cs
@@ -1,20 +1,12 @@
-﻿using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore;
+﻿using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Serilog;
 using Serilog.Events;
-using Serilog.Extensions.Logging;
-using Microsoft.Extensions.Logging;
 
 namespace Sample.Authentication
 {
     public class Program
     {
-
 
         public static void Main(string[] args)
         {
@@ -34,11 +26,10 @@ namespace Sample.Authentication
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>()
                 .ConfigureLogging((b)=> {
-                    b.AddSerilog(Log.Logger);
-                  //  b.AddConsole();
+                    b.AddSerilog(Log.Logger); // this does!
                 })
                 .UseUrls("http://*:5000", "http://*:5001", "http://*:5002", "http://*:5003", "http://*:5004")
-               // .UseSerilog(providers: Providers)
+               // .UseSerilog(providers: Providers) // this doesn't work with dotnettency!
                 .Build();
     }
 }
